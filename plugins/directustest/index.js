db63289ae9597dae3db19c27a03fb3c4cd52fecd
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-06-08 12:14:28 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -1,5 +1,12 @@
 module.exports = {
-  onPreBuild: () => {
-    console.log('Hello world from onPreBuild event!')
-  },
-}
+    onPreBuild: async ({ utils, packageJson }) => {
+    console.log("my plugin loaded!")
+        //import modules and define some terms
+        try {
+        //get sanity data
+        //then output as md files in our content folder
+        } catch (error) {
+            utils.build.failBuild('Failure message', { error })
+          }
+        },
+    }
