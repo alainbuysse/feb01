7d7a844f19e4f723c360e23cf7de6a8cda620d9a
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-06-08 12:20:55 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -1,6 +1,28 @@
 module.exports = {
     onPreBuild: async ({ utils, packageJson }) => {
     console.log("my plugin loaded!")
+        const fs = require("fs-extra");
+        
+    //add any serializers for your portable text
+    const serializers = {
+      types: {
+        code: (props) =>
+          "```" + props.node.language + "\n" + props.node.code + "\n```",
+      },
+    };
+        
+    fs.readdir("./content", (err, files) => {
+      if (err) console.log(err);
+      else {
+        files.forEach((file) => {
+          console.log(`Deleting: ${file}`);
+          fs.unlink(`content//${file}`, (err) => {
+            if (err) throw err;
+          });
+        });
+      }
+    });
+    
         //import modules and define some terms
         try {
         //get sanity data
