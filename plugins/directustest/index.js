4878be479525f9443af65fab106715279711e59c
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-07-13 21:50:12 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -25,23 +25,6 @@ module.exports = {
     
         //import modules and define some terms
         try {
-      
-    await fetch('https://cms.bimp.be/items/blog').then(response =>{
-        console.log('response: ',response);
-        return response.json(); 
-    }).then((data => {
-        console.log(data);
-        fs.writeFile("./content/blog/blabla.md", "dummy text", function (err) {
-          if (err) return console.log(err);
-            console.log('file written');
-          });
-      
-      fs.writeFile("./content/blog/text.csv", "dummy text", function (err) {
-          if (err) return console.log(err);
-            console.log('file written');
-          });
-      
-    }))
   
         } catch (error) {
             utils.build.failBuild('Failure message', { error })
