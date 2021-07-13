906d0691613a71f427f18ff6813f3e7fb486b39a
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-07-13 21:55:49 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -1,33 +1,44 @@
 module.exports = {
     onPreBuild: async ({ utils, packageJson }) => {
-    console.log("my plugin loaded!")
+        console.log("my plugin loaded!")
         const fs = require("fs-extra");
+       
         
-    //add any serializers for your portable text
-    const serializers = {
-      types: {
-        code: (props) =>
-          "```" + props.node.language + "\n" + props.node.code + "\n```",
-      },
-    };
-        
-    fs.readdir("./content", (err, files) => {
-      if (err) console.log(err);
-      else {
-        files.forEach((file) => {
-          console.log(`Deleting: ${file}`);
-          fs.unlink(`content//${file}`, (err) => {
-            if (err) throw err;
-          });
-        });
-      }
+        fs.readdir("./content", (err, files) => {
+          if (err) console.log(err);
+          else {
+            files.forEach((file) => {
+              console.log(`Deleting: ${file}`);
+              fs.unlink(`content//${file}`, (err) => {
+                if (err) throw err;
+              });
+            });
+          }
     });
     
         //import modules and define some terms
         try {
-  
-        } catch (error) {
+            
+            await fetch('https://cms.bimp.be/items/blog').then(response =>{
+        console.log('response: ',response);
+        return response.json(); 
+    }).then((data => {
+        console.log(data);
+        fs.writeFile("./content/blog/blabla.md", "dummy text", function (err) {
+          if (err) return console.log(err);
+            console.log('file written');
+          });
+      
+      fs.writeFile("./content/blog/text.csv", "dummy text", function (err) {
+          if (err) return console.log(err);
+            console.log('file written');
+          });
+      
+    }))
+            
+        } 
+        catch (error) {
             utils.build.failBuild('Failure message', { error })
-          }
-        },
-    }
+        }
+    },
+}
